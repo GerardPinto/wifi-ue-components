ddcc577486ff48f9d0a68363e7777c8ed5e8f9f4
Gerard Pinto <gerardfly9@gmail.com>
2017-11-19 19:37:52 -0800

    Extracted IKASMP packet

--- tests/packet-analyzer.py
+++ tests/packet-analyzer.py
@@ -3,4 +3,17 @@
 import pyshark
 
 cap = pyshark.FileCapture('../data/wifi-calling-android-t-mobile.pcapng')
-print cap[0]
+
+# print dir(cap[1282].frame_info.protocols)
+# if hasattr(cap[1282], 'isakmp'):
+#     print dir(cap[1282].isakmp)
+
+print "[+] Initiator SPI \t %s " % (cap[1282].isakmp.ispi)
+print "[+] Key Exchange Data %s " % (cap[1282].isakmp.key_exchange_data)
+print "[+] Nonce %s " % (cap[1282].isakmp.nonce)
+# for index in range(0, 9248):
+# 	protocols = cap[index].frame_info.protocols.showname_value
+# 	data = protocols.split(":")
+# 	if "isakmp" in data:
+# 		print index
+# 		break
